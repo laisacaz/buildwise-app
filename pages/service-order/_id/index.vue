7a500bf156b70a08ec07bcfbc1a5592f50761373
Laisa Cazarotto <laisacaz@hotmail.com>
2024-05-09 00:01:26 -0300

    Adiciona pagina de serviços

---
+++ pages/service-order/_id/index.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <v-row class="mt-4">
+      <h1 class="ml-4">
+        {{ isEditing ? "Edição de serviço" : "Cadastro de serviço" }}
+      </h1>
+    </v-row>
+    <v-card style="max-width: 800px" class="mt-6 mb-6" outlined>
+      <v-row class="ml-2 mt-2 mr-0">
+        <v-col cols="8" class="mr-0">
+          <v-text-field
+            v-model="fields.description"
+            clearable
+            dense
+            class="required"
+            :maxlength="50"
+            label="Descrição"
+            placeholder="Descrição"
+            outlined
+          >
+          </v-text-field>
+        </v-col>
+      </v-row>
+
+      <v-row class="ml-2 mt-2">
+        <v-col cols="auto">
+          <v-currency-field
+            v-model.number="fields.price"
+            style="max-width: 126px"
+            dense
+            class="required"
+            type="number"
+            label="Preço"
+            outlined
+          >
+          </v-currency-field>
+        </v-col>
+      </v-row>
+      <v-row justify="end" class="ml-2 mt-2 mb-2">
+        <v-col cols="auto">
+          <v-btn class="ml-8" color="white black--text" @click="leave">
+            Voltar
+          </v-btn>
+        </v-col>
+        <v-col cols="auto" class="mr-10">
+          <v-btn class="ml-8" color="primary" @click="save"> Salvar </v-btn>
+        </v-col>
+      </v-row>
+    </v-card>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+import { IServiceOrder } from "~/utils/interfaces/crudObjects";
+export default Vue.extend({
+  data() {
+    return {
+      serviceId: 0,
+      isEditing: false,
+      mensagemAlerta: false,
+      fields: {
+        description: "",
+        price: 0,
+      } as IServiceOrder,
+    };
+  },
+  created() {
+    this.startPage();
+  },
+  methods: {
+    startPage() {
+      const id = parseInt(this.$route.params.id);
+      if (!isNaN(id)) {
+        this.getData(id);
+        this.serviceId = id;
+      }
+    },
+    leave() {
+      this.$router.push("/service");
+    },
+    async getData(serviceId: number) {
+      this.isEditing = true;
+      await this.$axios
+        .get<IServiceOrder>("/service/" + serviceId, {
+          headers: {
+            "content-type": "application/json",
+            accept: "application/json",
+          },
+        })
+        .then((response) => {
+          this.fields = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async save() {
+      if (this.isEditing) {
+        this.edit(parseInt(this.$route.params.id));
+      } else {
+        await this.$axios
+          .post<number>("/service", this.fields)
+          .then(() => {
+            this.$globalFunctions.successAlert(
+              "Serviço salvo com sucesso",
+              5000
+            );
+            this.leave();
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+    async edit(id: number) {
+      await this.$axios
+        .put("/service/" + id, this.fields)
+        .then(() => {
+          this.$globalFunctions.successAlert("Serviço salvo com sucesso", 5000);
+          this.leave();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+});
+</script>
--- utils/interfaces/crudObjects.ts
+++ utils/interfaces/crudObjects.ts
@@ -1,160 +1,173 @@
-import  { EConstructionSearchType, EPersonSearchType, EProductSearchType, ESaleReceivementMethod, ESaleSearchType, EStatusConstruction, EStatusSale } from '../enumerators/enum'
+import {
+  EConstructionSearchType,
+  EPersonSearchType,
+  EProductSearchType,
+  ESaleReceivementMethod,
+  ESaleSearchType,
+  EStatusConstruction,
+  EStatusSale,
+} from "../enumerators/enum";
 
 //SEARCH
 export interface defaultSearchResponse<reponseObjectType = any> {
-    data: Array<reponseObjectType>
-    pageNumber?: number
-    pageSize?: number
-    totalPages?: number
-    totalRecords?: number
-    firstPageUri?: string
-    lastPageUri?: string
-    nextPageUri?: string
-    previousPageUri?: string
-  }
-  
-export interface IProductSearchParams {
-    search?: string
-    searchType: EProductSearchType,
-    pageNumber: number
-    pageSize: number
-}
-export interface IProductSearchResponse {
-    id: number,
-    description: string,
-    reference: string,
-    stockQuantity: number,
-    price: number
-    selectedBtn?: boolean  
-}
-export interface IPersonSearchParams {
-    searchType: EPersonSearchType 
-    search?: string
-    id?: number
-    
-}
-export interface IPersonSearchResponse {
-    id: number
-    name: string,
-    identityNumber?: string
-}
-export interface IConstructionSearchParams{
-    id?: number
-    status?: EStatusConstruction
-    searchType: EConstructionSearchType
-    search?: string
-}
-export interface IConstructionSearchResponse{
-    id: number
-    createdAt: string
-    status: EStatusConstruction
-    clientName: string
-    selectedBtn?: boolean 
-    statusDescription: string
-}
-export interface ISaleSearchParams{
-    id?: number
-    status?: EStatusSale
-    searchType: ESaleSearchType
-    search?: string
-    total: number
-}
-export interface ISaleSearchResponse{
-    id: number
-    createdAt: string
-    status: ESaleSearchType
-    clientName: string
-    total: number
-    statusDescription: string
+  data: Array<reponseObjectType>;
+  pageNumber?: number;
+  pageSize?: number;
+  totalPages?: number;
+  totalRecords?: number;
+  firstPageUri?: string;
+  lastPageUri?: string;
+  nextPageUri?: string;
+  previousPageUri?: string;
 }
 
-export interface IReportSaleByPeriodParams{
-    startDate: string,
-    endDate: string
+export interface IProductSearchParams {
+  search?: string;
+  searchType: EProductSearchType;
+  pageNumber: number;
+  pageSize: number;
+}
+export interface IProductSearchResponse {
+  id: number;
+  description: string;
+  reference: string;
+  stockQuantity: number;
+  price: number;
+  selectedBtn?: boolean;
+}
+export interface IPersonSearchParams {
+  searchType: EPersonSearchType;
+  search?: string;
+  id?: number;
+}
+export interface IPersonSearchResponse {
+  id: number;
+  name: string;
+  identityNumber?: string;
+}
+export interface IConstructionSearchParams {
+  id?: number;
+  status?: EStatusConstruction;
+  searchType: EConstructionSearchType;
+  search?: string;
+}
+export interface IConstructionSearchResponse {
+  id: number;
+  createdAt: string;
+  status: EStatusConstruction;
+  clientName: string;
+  selectedBtn?: boolean;
+  statusDescription: string;
+}
+export interface ISaleSearchParams {
+  id?: number;
+  status?: EStatusSale;
+  searchType: ESaleSearchType;
+  search?: string;
+  total: number;
+}
+export interface ISaleSearchResponse {
+  id: number;
+  createdAt: string;
+  status: ESaleSearchType;
+  clientName: string;
+  total: number;
+  statusDescription: string;
+}
+
+export interface IReportSaleByPeriodParams {
+  startDate: string;
+  endDate: string;
 }
 //INSERT
 
 export interface IProduct {
-    id?: number
-    description: string
-    reference: string
-    stockQuantity: number
-    barCode?: string
-    price: number
-    cost?: number
+  id?: number;
+  description: string;
+  reference: string;
+  stockQuantity: number;
+  barCode?: string;
+  price: number;
+  cost?: number;
 }
-export interface ISimpleProduct{
-    productId: number
-    stockQuantity: number
+export interface ISimpleProduct {
+  productId: number;
+  stockQuantity: number;
 }
-export interface IProductSale{
-    productId: number
-    stockQuantitySale: number
-    reference: string
-    description: string
-    price: number
+export interface IProductSale {
+  productId: number;
+  stockQuantitySale: number;
+  reference: string;
+  description: string;
+  price: number;
 }
 export interface IPerson {
-    id?: number
-    name: string
-    identityNumber?: string
-    socialSecurityNumber?: string
-    cellphone?: string
-    address: IAddress    
+  id?: number;
+  name: string;
+  identityNumber?: string;
+  socialSecurityNumber?: string;
+  cellphone?: string;
+  address: IAddress;
 }
 export interface ISimplePerson {
-    id: number
-    name: string    
-    status: boolean
+  id: number;
+  name: string;
+  status: boolean;
 }
 export interface IAddress {
-    id?: number
-    zipCode: string
-    state: string
-    city: string
-    street?: string
-    streetNumber: string
-    district: string
+  id?: number;
+  zipCode: string;
+  state: string;
+  city: string;
+  street?: string;
+  streetNumber: string;
+  district: string;
 }
 export interface IConstruction {
-    client: ISimplePerson
-    id?: number
-    clientId?: number
-    observation?: string
-    address: IAddress
-    status: EStatusConstruction
+  client: ISimplePerson;
+  id?: number;
+  clientId?: number;
+  observation?: string;
+  address: IAddress;
+  status: EStatusConstruction;
 }
 export interface ISale {
-    id?: number
-    clientId: number
-    sellerId: number
-    constructionId?: number
-    products: ISimpleProduct []
-    receivementMethod?: ESaleReceivementMethod
-    discount?: number
-    increase?: number
-    observation?: string
-    statusDescription: string
-    status: EStatusSale
-    createdAt?: string
-    finalizedAt?: string
-    paidAmount?: number
-    total?: number
-    subtotal: number
+  id?: number;
+  clientId: number;
+  sellerId: number;
+  constructionId?: number;
+  products: ISimpleProduct[];
+  receivementMethod?: ESaleReceivementMethod;
+  discount?: number;
+  increase?: number;
+  observation?: string;
+  statusDescription: string;
+  status: EStatusSale;
+  createdAt?: string;
+  finalizedAt?: string;
+  paidAmount?: number;
+  total?: number;
+  subtotal: number;
 }
 export interface IFinishSale {
-    receivementMethod?: ESaleReceivementMethod
-    discount?: number
-    increase?: number
-    paidAmount?: number
-    total?: number
-    subtotal?: number
+  receivementMethod?: ESaleReceivementMethod;
+  discount?: number;
+  increase?: number;
+  paidAmount?: number;
+  total?: number;
+  subtotal?: number;
 }
 
-export interface ISaleById{
-    sale: ISale
-    client: ISimplePerson
-    seller: ISimplePerson
-    products: IProductSale []
-    construction?: IConstruction
+export interface ISaleById {
+  sale: ISale;
+  client: ISimplePerson;
+  seller: ISimplePerson;
+  products: IProductSale[];
+  construction?: IConstruction;
+}
+export interface IServiceOrder {
+  id?: number;
+  description: string;
+  status: boolean;
+  price: number;
 }
