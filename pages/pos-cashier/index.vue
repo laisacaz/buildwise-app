fd38d9f17658133073115c01a0a2e18eab7f5aa7
laisacaz <laisacaz@hotmail.com>
2024-05-28 22:20:21 -0300

    Adiciona tela de caixa

---
+++ pages/pos-cashier/index.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <v-row>
+        <v-col cols="6">
+            <v-card outlined>            
+                <v-card-title>
+                    Vendas
+                </v-card-title>
+            <v-data-table>
+            </v-data-table>
+            </v-card>
+        </v-col>
+        <v-col cols="6">
+            <v-card outlined>
+                <v-card-title>
+                    Serviços
+                </v-card-title>
+            <v-data-table>
+            </v-data-table>
+            </v-card>
+        </v-col>
+    </v-row>
+    <v-divider class="mt-4 mb-4"></v-divider>
+    <v-row>
+        <v-col cols="auto">
+            <v-btn>Entrada</v-btn>
+        </v-col>
+         <v-col cols="auto">
+            <v-btn>Saída</v-btn>
+        </v-col>
+    </v-row>
+
+</div>
+</template>
