b0ebbee9146fc1209d5caf538d20bd5009f46081
Laisa Cazarotto <laisacaz@hotmail.com>
2024-06-26 00:19:28 -0300

    Adiciona fluxo de caixa

--- pages/pos-cashier/index.vue
+++ pages/pos-cashier/index.vue
@@ -1,40 +1,51 @@
 <template>
   <div class="mt-4">
-    <v-form v-if="showOpenCashierConfirmDialog">
+    <v-form v-if="showOpenCashierConfirmDialog" style="max-width: 280px">
       <v-dialog
         v-model="showOpenCashierConfirmDialog"
         persistent
-        max-width="400px"
+        max-width="280px"
       >
-        <v-card class="no-scroll">
-          <v-row>
+        <v-card class="no-scroll" style="max-width: 280px">
+          <v-row no-gutters>
             <v-col cols="auto">
               <v-card-title> Insira o valor de abertura </v-card-title>
             </v-col>
           </v-row>
           <v-row no-gutters>
-            <v-col cols="6">
-              <v-currency-field outlined class="ml-4"></v-currency-field>
+            <v-col cols="11">
+              <v-currency-field
+                v-model="initialValue"
+                outlined
+                type="number"
+                class="ml-4"
+              ></v-currency-field>
             </v-col>
           </v-row>
-          <v-row justify="end" class="mt-4" no-gutters>
+          <v-row class="mt-4" no-gutters>
             <v-col cols="auto">
-              <v-btn class="ml-8 mb-4" text color="black">
+              <v-btn class="ml-4 mb-4" text color="black" @click="leave">
                 <v-icon class="mr-2">mdi-arrow-right-bottom</v-icon>
                 Voltar
               </v-btn>
             </v-col>
-            <v-col cols="auto" class="mr-10">
-              <v-btn class="ml-8 mb-4" color="primary"> Abrir </v-btn>
+            <v-col cols="auto" class="mr-10" @click="openCashier">
+              <v-btn class="ml-5 mb-4" color="primary"> Abrir </v-btn>
             </v-col>
           </v-row>
         </v-card>
       </v-dialog>
     </v-form>
+    <PDF-Viewer
+      v-model="showPdf"
+      title="PDF"
+      :pdf-file.sync="thePDFFileBlobRoute"
+    ></PDF-Viewer>
     <pop-up-confirmation
       v-model="showOpenCashierDialog"
       message="Deseja abrir um novo caixa?"
       confirm-message="Abrir"
+      cancel-message="Voltar"
       title="Caixa fechado!"
       @confirmClick="showOpenCashierConfirmDialog = true"
       @cancelClick="leave"
@@ -45,10 +56,12 @@
         <v-card v-if="isCashierOpened">
           <v-row justify="end">
             <v-col cols="auto">
-              <v-icon>mdi-printer</v-icon>
+              <v-icon @click="print">mdi-printer</v-icon>
             </v-col>
             <v-col cols="auto">
-              <v-btn class="mr-2">Fechar caixa</v-btn>
+              <v-btn class="mr-2" color="primary" @click="closeCashier"
+                >Fechar caixa</v-btn
+              >
             </v-col>
           </v-row>
           <v-card-title class="mt-n15"
@@ -108,8 +121,11 @@ export default Vue.extend({
     return {
       showOpenCashierDialog: false,
       showOpenCashierConfirmDialog: false,
+      showPdf: false,
+      thePDFFileBlobRoute: undefined as string | undefined,
       isCashierOpened: false,
       cashierId: 0,
+      initialValue: 0,
       cashier: {
         values: {
           amountAvailable: 0,
@@ -128,6 +144,43 @@ export default Vue.extend({
     this.isLastCashierOpen();
   },
   methods: {
+    async closeCashier() {
+      await this.$axios
+        .put("/cashier/" + this.cashierId, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then(() => {
+          this.$globalFunctions.successAlert("Caixa fechado com sucesso", 5000);
+          this.leave();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    async openCashier() {
+      await this.$axios
+        .post<number>(
+          "/cashier",
+          { initialValue: this.initialValue },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          this.$globalFunctions.successAlert("Caixa aberto com sucesso", 5000);
+          this.cashierId = res.data;
+          this.getValues();
+          this.showOpenCashierConfirmDialog = false;
+          this.isCashierOpened = true;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
     async isLastCashierOpen() {
       await this.$axios
         .get<boolean>("/cashier/check-last-cashier", {
@@ -166,6 +219,21 @@ export default Vue.extend({
     },
     leave() {
       this.$router.push("/menu");
+    },
+    async print(id: number) {
+      this.showPdf = true;
+      const responseType = "blob";
+
+      await this.$axios
+        .get<Blob>("/cashier/report/" + this.cashierId, {
+          responseType: responseType,
+        })
+        .then((res) => {
+          this.thePDFFileBlobRoute = window.URL.createObjectURL(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
 });
