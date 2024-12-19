ae1ce89ba11bc728e1a1f7fd72210fa08353390d
Laisa Cazarotto <laisacaz@hotmail.com>
2024-12-19 08:16:17 -0300

    remove coiasas inuteis

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,27 +1,26 @@
 <template>
-    <v-app>
-      <v-main class="page-background">
-        <v-row
-          style="height: 100vh; overflow: hidden !important"
-          no-gutters
-          class="ma-0 pa-0"
-          align="center"
-          justify="center"
-        >
-          <v-col sm="12" md="3" class="pa-0 ma-0">
-            <Nuxt></Nuxt>
-          </v-col>
-        </v-row>
-      </v-main>
-    </v-app>
-  </template>
-  <script lang="ts">
-  import Vue from "vue"
-  export default Vue.extend({
-    data() {
-      return {}
-    },
-  })
-  </script>
-  <style lang="scss"></style>
-  
+  <v-app>
+    <v-main class="page-background">
+      <v-row
+        style="height: 100vh; overflow: hidden !important"
+        no-gutters
+        class="ma-0 pa-0"
+        align="center"
+        justify="center"
+      >
+        <v-col sm="12" md="3" class="pa-0 ma-0">
+          <Nuxt></Nuxt>
+        </v-col>
+      </v-row>
+    </v-main>
+  </v-app>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {};
+  },
+});
+</script>
+<style lang="scss"></style>
