ce2221048cf8ced3a3d7a72b9500a7a70da5a607
Laísa Casarotto <109436562+laisacaz@users.noreply.github.com>
2025-07-09 23:37:55 -0300

    refactor: Tela de cadastro de pessoa (#21)
    * Adiciona componente de container
    
    * Remove prop inutil de componente de pesquisa
    
    * Torna tela de pessoa responsivo
    
    * ajusta containers
    
    * Melhora funções

---
+++ components/GenericContainer.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card :outlined="outlined">
+    <v-card-title :style="collapsible ? 'cursor: pointer' : ''" @click="toggle">
+      <v-icon v-if="collapsible" class="mr-2">{{
+        isOpen ? "mdi-menu-down" : "mdi-menu-right"
+      }}</v-icon>
+      <span>{{ title }}</span>
+    </v-card-title>
+    <v-expand-transition>
+      <v-card-text v-show="isOpen">
+        <slot name="default"></slot>
+      </v-card-text>
+    </v-expand-transition>
+  </v-card>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    collapsible: {
+      type: Boolean,
+      default: false,
+    },
+    startOpen: {
+      type: Boolean,
+      default: true,
+    },
+    outlined: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      isOpen: this.startOpen,
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    toggle() {
+      if (this.collapsible) {
+        this.isOpen = !this.isOpen;
+      }
+    },
+  },
+});
+</script>
--- pages/person/_id/index.vue
+++ pages/person/_id/index.vue
@@ -1,190 +1,170 @@
 <template>
   <div>
-    <v-row class="mt-4">
-      <h2 class="ml-4">
+    <v-row class="mt-2" dense>
+      <h2 class="ml-1">
         {{ isEditing ? "Edição de pessoa" : "Cadastro de pessoa" }}
       </h2>
     </v-row>
-
-    <v-card class="mt-6 mb-6" outlined>
-      <v-row class="ml-2 mt-2">
-        <v-col>
-          <h3>Dados</h3>
-        </v-col>
-      </v-row>
-
-      <v-row class="ml-2 mt-2 mr-0">
-        <v-col cols="4">
-          <v-text-field
-            v-model="fields.name"
-            clearable
-            dense
-            :maxlength="50"
-            label="Nome"
-            class="required"
-            placeholder="Nome"
-            outlined
-          >
-          </v-text-field>
-        </v-col>
-        <v-col cols="auto" class="mr-0">
-          <v-text-field
-            v-model="fields.identityNumber"
-            clearable
-            dense
-            :maxlength="15"
-            label="CPF"
-            placeholder="CPF"
-            outlined
-          >
-          </v-text-field>
-        </v-col>
-        <v-col cols="auto">
-          <v-text-field
-            v-model="fields.socialSecurityNumber"
-            clearable
-            dense
-            :maxlength="10"
-            label="RG"
-            placeholder="RG"
-            outlined
-          >
-          </v-text-field>
-        </v-col>
-        <v-col cols="auto">
-          <v-text-field
-            v-model="fields.cellphone"
-            clearable
-            dense
-            :maxlength="11"
-            label="Celular"
-            placeholder="Celular"
-            outlined
-          >
-          </v-text-field>
-        </v-col>
-      </v-row>
-    </v-card>
-
-    <v-card>
-      <v-row
-        class="ml-2 mt-2"
-        style="cursor: pointer"
-        @click="isAddressOpen = !isAddressOpen"
-      >
-        <v-icon size="30">{{
-          isAddressOpen ? "mdi-menu-down" : "mdi-menu-right"
-        }}</v-icon>
-        <v-col>
-          <h3>Endereço</h3>
-        </v-col>
-      </v-row>
-
-      <v-expand-x-transition>
-        <v-card-text v-show="isAddressOpen">
-          <v-row class="ml-2 mt-2">
-            <v-col cols="3">
-              <v-text-field
-                v-model="fields.address.street"
-                clearable
-                dense
-                :maxlength="20"
-                label="Rua"
-                placeholder="Rua"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-            <v-col cols="2">
-              <v-text-field
-                v-model="fields.address.streetNumber"
-                clearable
-                dense
-                :maxlength="10"
-                label="Nº"
-                placeholder="Nº"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-            <v-col cols="auto">
-              <v-text-field
-                v-model="fields.address.district"
-                clearable
-                dense
-                :maxlength="20"
-                label="Bairro"
-                placeholder="Bairro"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-            <v-col cols="2">
-              <v-text-field
-                v-model="fields.address.zipCode"
-                clearable
-                dense
-                :maxlength="10"
-                label="CEP"
-                placeholder="CEP"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-          </v-row>
-          <v-row class="ml-2 mt-2">
-            <v-col cols="auto">
-              <v-text-field
-                v-model="fields.address.city"
-                clearable
-                dense
-                :maxlength="25"
-                label="Cidade"
-                placeholder="Cidade"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-            <v-col cols="auto">
-              <v-text-field
-                v-model="fields.address.state"
-                clearable
-                dense
-                :maxlength="25"
-                label="Estado"
-                placeholder="Estado"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-expand-x-transition>
-    </v-card>
-    <v-card>
-      <v-row justify="end" class="ml-2 mt-2 mb-2">
-        <v-col cols="auto">
-          <v-btn class="ml-8" color="white black--text" @click="leave">
-            Voltar
-          </v-btn>
-        </v-col>
-        <v-col cols="auto" class="mr-10">
-          <v-btn class="ml-8" color="primary" @click="save"> Salvar </v-btn>
-        </v-col>
-      </v-row>
-    </v-card>
+    <v-row dense>
+      <v-col cols="12">
+        <generic-container title="Dados" outlined>
+          <template #default>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="fields.name"
+                  clearable
+                  dense
+                  :maxlength="50"
+                  label="Nome"
+                  class="required"
+                  placeholder="Nome"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="4" lg="4">
+                <v-text-field
+                  v-model="fields.identityNumber"
+                  clearable
+                  dense
+                  :maxlength="15"
+                  label="CPF"
+                  placeholder="CPF"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="4">
+                <v-text-field
+                  v-model="fields.socialSecurityNumber"
+                  clearable
+                  dense
+                  :maxlength="10"
+                  label="RG"
+                  placeholder="RG"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="4">
+                <v-text-field
+                  v-model="fields.cellphone"
+                  clearable
+                  dense
+                  :maxlength="11"
+                  label="Celular"
+                  placeholder="Celular"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+            </v-row>
+          </template>
+        </generic-container>
+      </v-col>
+    </v-row>
+    <v-row dense>
+      <v-col cols="12">
+        <generic-container title="Endereço" collapsible :start-open="false">
+          <template #default>
+            <v-row>
+              <v-col cols="12" sm="3" lg="2">
+                <v-text-field
+                  v-model="fields.address.zipCode"
+                  clearable
+                  dense
+                  :maxlength="10"
+                  label="CEP"
+                  placeholder="CEP"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="9" sm="6" lg="8">
+                <v-text-field
+                  v-model="fields.address.street"
+                  clearable
+                  dense
+                  :maxlength="20"
+                  label="Rua"
+                  placeholder="Rua"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="3" sm="3" lg="2">
+                <v-text-field
+                  v-model="fields.address.streetNumber"
+                  clearable
+                  dense
+                  :maxlength="10"
+                  label="Nº"
+                  placeholder="Nº"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="4" lg="4">
+                <v-text-field
+                  v-model="fields.address.district"
+                  clearable
+                  dense
+                  :maxlength="20"
+                  label="Bairro"
+                  placeholder="Bairro"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="4" lg="4">
+                <v-text-field
+                  v-model="fields.address.city"
+                  clearable
+                  dense
+                  :maxlength="25"
+                  label="Cidade"
+                  placeholder="Cidade"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="4" lg="4">
+                <v-text-field
+                  v-model="fields.address.state"
+                  clearable
+                  dense
+                  :maxlength="25"
+                  label="Estado"
+                  placeholder="Estado"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+            </v-row>
+          </template>
+        </generic-container>
+      </v-col>
+    </v-row>
+    <v-row justify="end">
+      <v-col cols="auto">
+        <v-btn color="white black--text" @click="leave"> Voltar </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn color="primary" @click="save"> Salvar </v-btn>
+      </v-col>
+    </v-row>
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
+import GenericContainer from "~/components/GenericContainer.vue";
 import { IAddress, IPerson } from "~/utils/interfaces/crudObjects";
 
 export default Vue.extend({
   data() {
     return {
       isEditing: false,
-      isAddressOpen: false,
-      personId: 0,
       fields: {
         name: "",
         identityNumber: "",
@@ -207,18 +187,17 @@ export default Vue.extend({
   methods: {
     startPage() {
       const id = parseInt(this.$route.params.id);
-      if (!isNaN(id)) {
-        this.getData(id);
-        this.personId = id;
+      this.isEditing = this.checkIfIsEditing(id);
+      if (this.isEditing) {
+        this.getPersonById(id);
       }
     },
-    leave() {
-      this.$router.push("/person");
+    checkIfIsEditing(id: number): boolean {
+      return !isNaN(id);
     },
-    async getData(personId: number) {
-      this.isEditing = true;
+    async getPersonById(id: number) {
       await this.$axios
-        .get<IPerson>("/person/" + personId, {
+        .get<IPerson>("/person/" + id, {
           headers: {
             "content-type": "application/json",
             accept: "application/json",
@@ -233,22 +212,9 @@ export default Vue.extend({
     },
     async save() {
       if (this.isEditing) {
-        if (this.isEditing) {
-          this.edit(parseInt(this.$route.params.id));
-        }
+        this.edit(parseInt(this.$route.params.id));
       } else {
-        await this.$axios
-          .post<number>("/person", this.fields)
-          .then((response) => {
-            this.$globalFunctions.successAlert(
-              "Pessoa salva com sucesso",
-              5000
-            );
-            this.leave();
-          })
-          .catch((error) => {
-            console.log(error);
-          });
+        this.insert();
       }
     },
     async edit(id: number) {
@@ -261,6 +227,20 @@ export default Vue.extend({
         .catch((error) => {
           console.log(error);
         });
+    },
+    async insert() {
+      await this.$axios
+        .post<number>("/person", this.fields)
+        .then(() => {
+          this.$globalFunctions.successAlert("Pessoa salva com sucesso", 5000);
+          this.leave();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    leave() {
+      this.$router.push("/person");
     },
   },
 });
