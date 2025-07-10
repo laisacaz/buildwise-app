84d53173b8a96de6d808e418219b6ce45887c475
Laisa Cazarotto <laisacaz@hotmail.com>
2025-07-09 23:23:37 -0300

    ajusta containers

--- pages/person/_id/index.vue
+++ pages/person/_id/index.vue
@@ -6,141 +6,145 @@
       </h2>
     </v-row>
     <v-row>
-      <generic-container title="Dados" outlined>
-        <template #default>
-          <v-row>
-            <v-col cols="12">
-              <v-text-field
-                v-model="fields.name"
-                clearable
-                dense
-                :maxlength="50"
-                label="Nome"
-                class="required"
-                placeholder="Nome"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-            <v-col cols="12" sm="4" lg="4">
-              <v-text-field
-                v-model="fields.identityNumber"
-                clearable
-                dense
-                :maxlength="15"
-                label="CPF"
-                placeholder="CPF"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-            <v-col cols="12" sm="4">
-              <v-text-field
-                v-model="fields.socialSecurityNumber"
-                clearable
-                dense
-                :maxlength="10"
-                label="RG"
-                placeholder="RG"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-            <v-col cols="12" sm="4">
-              <v-text-field
-                v-model="fields.cellphone"
-                clearable
-                dense
-                :maxlength="11"
-                label="Celular"
-                placeholder="Celular"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-          </v-row>
-        </template>
-      </generic-container>
+      <v-col cols="12">
+        <generic-container title="Dados" outlined>
+          <template #default>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="fields.name"
+                  clearable
+                  dense
+                  :maxlength="50"
+                  label="Nome"
+                  class="required"
+                  placeholder="Nome"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="4" lg="4">
+                <v-text-field
+                  v-model="fields.identityNumber"
+                  clearable
+                  dense
+                  :maxlength="15"
+                  label="CPF"
+                  placeholder="CPF"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="4">
+                <v-text-field
+                  v-model="fields.socialSecurityNumber"
+                  clearable
+                  dense
+                  :maxlength="10"
+                  label="RG"
+                  placeholder="RG"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="4">
+                <v-text-field
+                  v-model="fields.cellphone"
+                  clearable
+                  dense
+                  :maxlength="11"
+                  label="Celular"
+                  placeholder="Celular"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+            </v-row>
+          </template>
+        </generic-container>
+      </v-col>
     </v-row>
     <v-row>
-      <generic-container title="Endereço" collapsible :start-open="false">
-        <template #default>
-          <v-row>
-            <v-col cols="12" sm="3" lg="2">
-              <v-text-field
-                v-model="fields.address.zipCode"
-                clearable
-                dense
-                :maxlength="10"
-                label="CEP"
-                placeholder="CEP"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-            <v-col cols="9" sm="6" lg="8">
-              <v-text-field
-                v-model="fields.address.street"
-                clearable
-                dense
-                :maxlength="20"
-                label="Rua"
-                placeholder="Rua"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-            <v-col cols="3" sm="3" lg="2">
-              <v-text-field
-                v-model="fields.address.streetNumber"
-                clearable
-                dense
-                :maxlength="10"
-                label="Nº"
-                placeholder="Nº"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-            <v-col cols="12" sm="4" lg="4">
-              <v-text-field
-                v-model="fields.address.district"
-                clearable
-                dense
-                :maxlength="20"
-                label="Bairro"
-                placeholder="Bairro"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-            <v-col cols="12" sm="4" lg="4">
-              <v-text-field
-                v-model="fields.address.city"
-                clearable
-                dense
-                :maxlength="25"
-                label="Cidade"
-                placeholder="Cidade"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-            <v-col cols="12" sm="4" lg="4">
-              <v-text-field
-                v-model="fields.address.state"
-                clearable
-                dense
-                :maxlength="25"
-                label="Estado"
-                placeholder="Estado"
-                outlined
-              >
-              </v-text-field>
-            </v-col>
-          </v-row>
-        </template>
-      </generic-container>
+      <v-col cols="12">
+        <generic-container title="Endereço" collapsible :start-open="false">
+          <template #default>
+            <v-row>
+              <v-col cols="12" sm="3" lg="2">
+                <v-text-field
+                  v-model="fields.address.zipCode"
+                  clearable
+                  dense
+                  :maxlength="10"
+                  label="CEP"
+                  placeholder="CEP"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="9" sm="6" lg="8">
+                <v-text-field
+                  v-model="fields.address.street"
+                  clearable
+                  dense
+                  :maxlength="20"
+                  label="Rua"
+                  placeholder="Rua"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="3" sm="3" lg="2">
+                <v-text-field
+                  v-model="fields.address.streetNumber"
+                  clearable
+                  dense
+                  :maxlength="10"
+                  label="Nº"
+                  placeholder="Nº"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="4" lg="4">
+                <v-text-field
+                  v-model="fields.address.district"
+                  clearable
+                  dense
+                  :maxlength="20"
+                  label="Bairro"
+                  placeholder="Bairro"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="4" lg="4">
+                <v-text-field
+                  v-model="fields.address.city"
+                  clearable
+                  dense
+                  :maxlength="25"
+                  label="Cidade"
+                  placeholder="Cidade"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="4" lg="4">
+                <v-text-field
+                  v-model="fields.address.state"
+                  clearable
+                  dense
+                  :maxlength="25"
+                  label="Estado"
+                  placeholder="Estado"
+                  outlined
+                >
+                </v-text-field>
+              </v-col>
+            </v-row>
+          </template>
+        </generic-container>
+      </v-col>
     </v-row>
     <v-row justify="end">
       <v-col cols="auto">
